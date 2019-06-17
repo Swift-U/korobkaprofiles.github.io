3beb01ef2681864713d436e4df4479a0bc142f47
Родион Швырёв <rodionsvyrev@iMac-Rodion.local>
2019-06-18 02:01:07 +0300

    44

--- korobka/scrupt_services.js
+++ korobka/scrupt_services.js
@@ -2,11 +2,11 @@ var header = document.getElementById("buttonsDiv");
 var btns = header.getElementsByClassName("nav_link");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active_two");
+  var current = document.getElementsByClassName("active_twoo");
   if (current.length > 0) { 
-    current[0].className = current[0].className.replace(" active_two", "");
+    current[0].className = current[0].className.replace(" active_twoo", "");
   }
-  this.className += " active_two";
+  this.className += " active_twoo";
   });
 }
 function openNav() {
