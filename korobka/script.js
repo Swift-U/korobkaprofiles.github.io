c42c396fc708881240f15e90ab1f996ad1465355
Родион Швырёв <rodionsvyrev@iMac-Rodion.local>
2019-07-05 00:02:52 +0300

    44

--- korobka/script.js
+++ korobka/script.js
@@ -17,42 +17,42 @@ function addListeners(){
 document.querySelector("#button1").addEventListener("click",showImagesAlbum(myPicturesArray1))
 document.querySelector("#button2").addEventListener("click",showImagesAlbum(myPicturesArray2))
 document.querySelector("#button3").addEventListener("click",showImagesAlbum(myPicturesArray3))
-document.querySelector("#button4").addEventListener("click",showImagesAlbum(myPicturesArray4))
+// document.querySelector("#button4").addEventListener("click",showImagesAlbum(myPicturesArray4))
 document.querySelector("#button5").addEventListener("click",showImagesAlbum(myPicturesArray5));
 }function showImagesAlbum(i){return function(){console.
 log("invoked by showImages");let e=document.querySelector("#albumDiv");e.innerHTML="",i.forEach((i,o,a)=>{let t=document.createElement("img");t.src=i.thumbnailUrl,t.alt=i.title,t.title=i.title,t.id=i.id,t.addEventListener("click",showFullImage(i)),t.classList.add("picHold"),e.append(t)}),showFullImage(i[0])()}}function showFullImage(i){return function(){let e=document.querySelector("#fullImg");e.src=i.url,e.classList.add("fullImgClass")}}window.onload=function(){addListeners(),showImagesAlbum(myPicturesArray1)()};
 class PicHolder{constructor(i,e,o,a,t){this.albumId=i,this.id=e,this.title=o,this.url=a,this.thumbnailUrl=t}}let
 myPicturesArray1=[],myPicturesArray2=[], myPicturesArray3=[],myPicturesArray4=[],myPicturesArray5=[];
-myPicturesArray1[0]=new PicHolder(1,2,"1-0","https://mir-s3-cdn-cf.behance.net/project_modules/1400/8227a279600985.5cc85d14590d8.png "," https://mir-s3-cdn-cf.behance.net/project_modules/1400/8227a279600985.5cc85d14590d8.png"),
-myPicturesArray1[1]=new PicHolder(1,1,"1-1"," https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg"," https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg"),
-myPicturesArray1[2]=new PicHolder(1,3,"1-2","https://mir-s3-cdn-cf.behance.net/project_modules/1400/8227a279600985.5cc85d14590d8.png "," https://mir-s3-cdn-cf.behance.net/project_modules/1400/8227a279600985.5cc85d14590d8.png"),
-myPicturesArray1[3]=new PicHolder(1,4,"1-3"," https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg"," https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg"),
-myPicturesArray1[4]=new PicHolder(1,5,"1-4","https://mir-s3-cdn-cf.behance.net/project_modules/1400/8227a279600985.5cc85d14590d8.png ","https://mir-s3-cdn-cf.behance.net/project_modules/1400/8227a279600985.5cc85d14590d8.png "),
-myPicturesArray1[5]=new PicHolder(1,6,"1-5"," https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg","https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg "),
-myPicturesArray2[0]=new PicHolder(1,1,'2-0)'," "," "),
-myPicturesArray2[1]=new PicHolder(1,2,"2-1"," "," "),
-myPicturesArray2[2]=new PicHolder(1,3,"2-2"," "," "),
-myPicturesArray2[3]=new PicHolder(1,3,"2-3"," "," "),
-myPicturesArray2[4]=new PicHolder(1,5,"2-4"," "," "),
-myPicturesArray2[5]=new PicHolder(1,6,"2-5"," "," "),
-myPicturesArray3[0]=new PicHolder(1,1,"3-0"," "," "),
-myPicturesArray3[1]=new PicHolder(1,2,"3-1"," "," "),
-myPicturesArray3[2]=new PicHolder(1,3,"3-2"," "," "),
-myPicturesArray3[3]=new PicHolder(1,4,"3-3"," "," "),
-myPicturesArray3[4]=new PicHolder(1,5,'"3-4'," "," "),
-myPicturesArray3[5]=new PicHolder(1,6,"3-5","",""),
-myPicturesArray4[0]=new PicHolder(1,2,"4-0"," "," "),
-myPicturesArray4[1]=new PicHolder(1,1,"4-1","",""),
-myPicturesArray4[2]=new PicHolder(1,3,"4-2"," "," "),
-myPicturesArray4[3]=new PicHolder(1,4,"4-3"," "," "),
-myPicturesArray4[5]=new PicHolder(1,6,"4-5"," "," "),
-myPicturesArray4[4]=new PicHolder(1,5,"4-4"," "," ")
-myPicturesArray5[0]=new PicHolder(1,2,"5-0","https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg","https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg"),
-myPicturesArray5[1]=new PicHolder(1,1,"5-1","https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg","https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg"),
-myPicturesArray5[2]=new PicHolder(1,3,"5-2","https://mir-s3-cdn-cf.behance.net/project_modules/1400/8227a279600985.5cc85d14590d8.png","https://mir-s3-cdn-cf.behance.net/project_modules/1400/8227a279600985.5cc85d14590d8.png"),
-myPicturesArray5[3]=new PicHolder(1,4,"5-3","https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg","https://mir-s3-cdn-cf.behance.net/project_modules/1400/f4f3ae79600985.5cc85d1457ec0.jpg"),
-myPicturesArray5[4]=new PicHolder(1,5,"5-4","https://mir-s3-cdn-cf.behance.net/project_modules/1400/f8c21479600985.5cc85d1455a67.png","https://mir-s3-cdn-cf.behance.net/project_modules/1400/f8c21479600985.5cc85d1455a67.png"),
-myPicturesArray5[5]=new PicHolder(1,6,"5-5","https://mir-s3-cdn-cf.behance.net/project_modules/1400/8227a279600985.5cc85d14590d8.png","https://mir-s3-cdn-cf.behance.net/project_modules/1400/8227a279600985.5cc85d14590d8.png");
+myPicturesArray1[0]=new PicHolder(1,2,"1-0","Img/body/Caviar.jpg"," Img/body/Caviar.jpg"),
+myPicturesArray1[1]=new PicHolder(1,1,"1-1"," Img/body/Caviar.jpg"," Img/body/Caviar.jpg"),
+myPicturesArray1[2]=new PicHolder(1,3,"1-2","Img/body/Caviar.jpg"," Img/body/Caviar.jpg"),
+myPicturesArray1[3]=new PicHolder(1,4,"1-3"," Img/body/Caviar.jpg","Img/body/Caviar.jpg"),
+myPicturesArray1[4]=new PicHolder(1,5,"1-4","Img/body/Caviar.jpg","Img/body/Caviar.jpg "),
+myPicturesArray1[5]=new PicHolder(1,6,"1-5"," Img/body/Caviar.jpg","Img/body/Caviar.jpg"),
+myPicturesArray2[0]=new PicHolder(1,1,'2-0)'," Img/body/Arz.jpg"," Img/body/Arz.jpg"),
+myPicturesArray2[1]=new PicHolder(1,2,"2-1","Img/body/Arz.jpg "," Img/body/Arz.jpg"),
+myPicturesArray2[2]=new PicHolder(1,3,"2-2"," Img/body/Arz.jpg"," Img/body/Arz.jpg"),
+myPicturesArray2[3]=new PicHolder(1,3,"2-3"," Img/body/Arz.jpg"," Img/body/Arz.jpg"),
+myPicturesArray2[4]=new PicHolder(1,5,"2-4"," Img/body/Arz.jpg"," Img/body/Arz.jpg"),
+myPicturesArray2[5]=new PicHolder(1,6,"2-5","Img/body/Arz.jpg "," Img/body/Arz.jpg"),
+myPicturesArray3[0]=new PicHolder(1,1,"3-0"," Img/body/MeetUp.jpg"," Img/body/MeetUp.jpg"),
+myPicturesArray3[1]=new PicHolder(1,2,"3-1","Img/body/MeetUp.jpg ","Img/body/MeetUp.jpg "),
+myPicturesArray3[2]=new PicHolder(1,3,"3-2","Img/body/MeetUp.jpg ","Img/body/MeetUp.jpg "),
+myPicturesArray3[3]=new PicHolder(1,4,"3-3"," Img/body/MeetUp.jpg","Img/body/MeetUp.jpg "),
+myPicturesArray3[4]=new PicHolder(1,5,'"3-4',"Img/body/MeetUp.jpg "," Img/body/MeetUp.jpg"),
+myPicturesArray3[5]=new PicHolder(1,6,"3-5","Img/body/MeetUp.jpg","Img/body/MeetUp.jpg"),
+// myPicturesArray4[0]=new PicHolder(1,2,"4-0"," "," "),
+// myPicturesArray4[1]=new PicHolder(1,1,"4-1","",""),
+// myPicturesArray4[2]=new PicHolder(1,3,"4-2"," "," "),
+// myPicturesArray4[3]=new PicHolder(1,4,"4-3"," "," "),
+// myPicturesArray4[5]=new PicHolder(1,6,"4-5"," "," "),
+// myPicturesArray4[4]=new PicHolder(1,5,"4-4"," "," ")
+myPicturesArray5[0]=new PicHolder(1,2,"5-0","Img/body/Porto.jpg","Img/body/Porto.jpg"),
+myPicturesArray5[1]=new PicHolder(1,1,"5-1","Img/body/Porto.jpg","Img/body/Porto.jpg"),
+myPicturesArray5[2]=new PicHolder(1,3,"5-2","Img/body/Porto.jpg","Img/body/Porto.jpg"),
+myPicturesArray5[3]=new PicHolder(1,4,"5-3","Img/body/Porto.jpg","Img/body/Porto.jpg"),
+myPicturesArray5[4]=new PicHolder(1,5,"5-4","Img/body/Porto.jpg","Img/body/Porto.jpg"),
+myPicturesArray5[5]=new PicHolder(1,6,"5-5","Img/body/Porto.jpg","Img/body/Porto.jpg");
 
 
   // 
